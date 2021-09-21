f72efb105f2b4068536ab0dd5a1616bd06950142
fabiovalenzuela <fvalenzuela1@ggc.edu>
2021-09-21 10:32:57 -0400

    corrected major errors in startup on mobile devices, installed stack properly, disables req cycle in auth and route classes

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -21,7 +21,7 @@ export default LoginScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignment: 'center',
+        alignItems: 'center',
         justifyContent: 'center'
     },
 });
