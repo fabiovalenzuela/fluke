c99a47a286016152d5181a64e4e95de3ac6b3e71
fabiovalenzuela <fvalenzuela1@ggc.edu>
2021-09-21 10:32:57 -0400

    corrected major errors in startup on mobile devices, installed stack properly, disables req cycle in auth and route classes

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -3,13 +3,15 @@ import {View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import SignupScreen from '../screens/SignUpScreen';
 import LoginScreen from "../screens/LoginScreen";
+// import Routes from "./Routes";
 
 
 const Auth = () => {
-    retun (
-        <Stack.Navigator initailRouteName={'Login'}>
-        <Stack.Screen name="Login" componen={LoginScreen} options={{header: () => null}} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        </Stack.Navigator>
+    return (
+        <stack.Navigator initailRouteName={'Login'}>
+        <stack.Screen name="Login" componen={LoginScreen} options={{header: () => null}} />
+        <stack.Screen name="Signup" component={SignupScreen} />
+        </stack.Navigator>
     );
 };
+export default Auth;
