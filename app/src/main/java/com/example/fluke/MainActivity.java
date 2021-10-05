926cdfa9f7d242bd1a9530c0c9a0c4facf1abec2
fabiovalenzuela <fvalenzuela1@ggc.edu>
2021-10-04 21:33:18 -0400

    adds client class to main

--- app/src/main/java/com/example/fluke/MainActivity.java
+++ app/src/main/java/com/example/fluke/MainActivity.java
@@ -7,12 +7,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.InetAddresses;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -21,8 +28,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.net.wifi.WifiManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
     List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
     String[] deviceNameArray;
     WifiP2pDevice[] devices;
+
+    Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +93,29 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final WifiP2pDevice device  = devices[i];
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+                manager.connect(channel,config,new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        connectionStatus.setText("Good Connect to" + device.deviceAddress);
+
+                    }
+
+                    @Override
+                    public void onFailure(int i) {
+                        connectionStatus.setText("Bad Connect");
+
+                    }
+                });
+
+            }
+        });
     }
 
 
@@ -100,7 +141,42 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
             if(wifiP2pDeviceList.equals(peers)){
-                
+                peers.clear();
+                peers.addAll(wifiP2pDeviceList.getDeviceList());
+
+                deviceNameArray = new String[wifiP2pDeviceList.getDeviceList().size()];
+                devices = new WifiP2pDevice[wifiP2pDeviceList.getDeviceList().size()];
+
+                int index = 0;
+                for(WifiP2pDevice device : wifiP2pDeviceList.getDeviceList()){
+
+                    deviceNameArray[index] = device.deviceName;
+                    devices[index] = device;
+                }
+
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,deviceNameArray);
+                listView.setAdapter(adapter);
+
+                if(peers.size() == 0){
+                    connectionStatus.setText("No Device Found");
+                    return;
+                }
+
+            }
+        }
+    };
+
+    WifiP2pManager.ConnectionInfoListener connectionInfoListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo wifiGroup) {
+            final InetAddress groupOwnerAddress = wifiGroup.groupOwnerAddress;
+            if(wifiGroup.groupFormed && wifiGroup.isGroupOwner)
+            {
+                connectionStatus.setText("host");
+            }else if (wifiGroup.groupFormed)
+            {
+                connectionStatus.setText("client");
             }
         }
     };
@@ -116,4 +192,66 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(receiver);
     }
+
+    public class ClientClass extends Thread {
+        String hostAdd;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+
+
+        public ClientClass(InetAddress hostAddress) {
+            hostAdd = hostAddress.getHostAddress();
+            socket= new Socket();
+
+        }
+
+        @Override
+        public void run(){
+            try{
+                socket.connect(new InetSocketAddress(hostAdd, 8888),500);
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+
+            }catch (IOException e){
+                e.printStackTrace();;
+            }
+
+
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            Handler handler = new Handler(Looper.getMainLooper());
+
+
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+
+                    byte[] buffer = new byte[1024];
+                    int bytes;
+
+                    while (socket!=null){
+                        try{
+                            bytes = inputStream.read(buffer);
+                            if(bytes>0){
+                                int finalBytes = bytes;
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        String tempMSG = new String(buffer,0,finalBytes);
+                                        msg_box.setText(tempMSG);
+
+
+                                    }
+                                });
+                            }
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                }
+            });
+        }
+    }
+
 }
--- app/src/main/java/com/example/fluke/WifiDirectBroadcastReceiver.java
+++ app/src/main/java/com/example/fluke/WifiDirectBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.example.fluke;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 
 public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
@@ -24,9 +25,22 @@ public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
 
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action))
         {
+            if(manager!=null)
+            {
+                manager.requestPeers(channel, activity.peerListListener);
+            }
 
         } else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action))
         {
+            if(manager!= null){
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+                if(networkInfo.isConnected()){
+                    manager.requestConnectionInfo(channel, activity.connectionInfoListener);
+                }
+                else{
+                    activity.connectionStatus.setText("Not connected");
+                }
+            }
 
         }
 
